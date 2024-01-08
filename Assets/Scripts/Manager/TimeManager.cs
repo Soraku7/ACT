2f750a0c630a0b04989ec57f2a2a4f8283fadbe0
Soraku7 <2673140287@qq.com>
2024-01-08 23:44:43 +0800

    TimeManager

---
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Unilts.Timer;
+using Unilts.Tools.Singleton;
+using UnityEngine;
+
+namespace Manager
+{
+    public class TimeManager : Singleton<TimeManager>
+    {
+        [SerializeField] private int _initMaxTimerCount = 0;
+
+        private Queue<GameTimer> _noWorkTimer = new Queue<GameTimer>();
+        private List<GameTimer> _workingTimer = new List<GameTimer>();
+
+        private void Start()
+        {
+            InitTimerManager();
+        }
+
+        private void Update()
+        {
+            UpdateWorkingTimer();
+        }
+
+        private void InitTimerManager()
+        {
+            for (int i = 0; i < _initMaxTimerCount; i++)
+            {
+                CreatTimer();
+            }
+        }
+
+        private void CreatTimer()
+        {
+            var timer = new GameTimer();
+            _noWorkTimer.Enqueue(timer);
+        }
+        
+        public void TryGetOneTimer(float time , Action task)
+        {
+            if (_noWorkTimer.Count == 0)
+            {
+                CreatTimer();
+                var timer = _noWorkTimer.Dequeue();
+                timer.StartTimer(time , task);
+                _workingTimer.Add(timer);
+            }
+            else
+            {
+                var timer = _noWorkTimer.Dequeue();
+                timer.StartTimer(time , task);
+                _workingTimer.Add(timer);
+            }
+        }
+
+        private void UpdateWorkingTimer()
+        {
+            if (_workingTimer.Count == 0) return;
+            for (var i = 0; i < _workingTimer.Count; i++)
+            {
+                if (_workingTimer[i].GetTimerState() == TimerState.Working)
+                {
+                    _workingTimer[i].UpdateTimer();
+                }
+                else
+                {
+                    //任务完成
+                    _noWorkTimer.Enqueue(_workingTimer[i]);
+                    _workingTimer[i].ResetTimer();
+                    _workingTimer.Remove(_workingTimer[i]);
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Unilts/Timer/GameTimer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unilts.Timer
+{
+    /// <summary>
+    /// 计时器状态
+    /// </summary>
+    public enum TimerState
+    {
+        Notworker,
+        Working,
+        Done
+    }
+
+    public class GameTimer
+    {
+        private float _startTime;
+        private Action _task;
+        private bool _isStopTimer;
+        private TimerState _timerState;
+
+        public GameTimer()
+        {
+            ResetTimer();
+        }
+
+        /// <summary>
+        /// 开始计时器
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="task"></param>
+        public void StartTimer(float time, Action task)
+        {
+            _startTime = time;
+            _task = task;
+            _isStopTimer = false;
+            _timerState = TimerState.Working;
+        }
+
+        /// <summary>
+        /// 更新计时器
+        /// </summary>
+        public void UpdateTimer()
+        {
+            if (_isStopTimer) return;
+            _startTime -= Time.deltaTime;
+            if (_startTime < 0f)
+            {
+                _task?.Invoke();
+                _timerState = TimerState.Done;
+                _isStopTimer = true;
+            }
+        }
+
+        public TimerState GetTimerState() => _timerState;
+
+        public void ResetTimer()
+        {
+            _startTime = 0f;
+            _task = null;
+            _isStopTimer = true;
+            _timerState = TimerState.Notworker;
+        }
+    }
+}
