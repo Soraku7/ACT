6f1a0a88b29d527512087454faa7211e494e5981
Soraku7 <2673140287@qq.com>
2024-01-18 12:51:41 +0800

    Audio

---
+++ Assets/Scripts/Manager/GamePoolManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Unilts.Tools.Singleton;
+using UnityEngine;
+
+namespace Manager
+{
+    public class GamePoolManager : Singleton<GamePoolManager>
+    {
+        [Serializable]
+        private class PoolItem
+        {
+            public string ItemName;
+            public GameObject Item;
+            public int InitMaxCount;
+        }
+
+        [SerializeField] private List<PoolItem> _configPoolItem = new List<PoolItem>();
+    }
+}
