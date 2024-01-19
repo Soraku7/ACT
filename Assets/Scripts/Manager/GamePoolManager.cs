3cbcfe31c226dcf49fa0055a3148c76e37032cd2
Soraku7 <2673140287@qq.com>
2024-01-19 23:08:02 +0800

    PoolManager

--- Assets/Scripts/Manager/GamePoolManager.cs
+++ Assets/Scripts/Manager/GamePoolManager.cs
@@ -16,5 +16,78 @@ namespace Manager
         }
 
         [SerializeField] private List<PoolItem> _configPoolItem = new List<PoolItem>();
+
+        private Dictionary<string, Queue<GameObject>> _poolCenter = new Dictionary<string, Queue<GameObject>>();
+
+        private GameObject _poolItemParent;
+
+        private void Start()
+        {
+            _poolItemParent = new GameObject("PoolItems");
+            _poolItemParent.transform.SetParent(this.transform);
+            InitPool();
+        }
+
+        
+        private void InitPool()
+        {
+            if (_configPoolItem.Count == 0) return;
+
+            for (var i = 0; i < _configPoolItem.Count; i++)
+            {
+                for (int j = 0; j < _configPoolItem[i].InitMaxCount; j ++)
+                {
+                    var item = Instantiate(_configPoolItem[i].Item, _poolItemParent.transform, true);
+                    item.SetActive(false);
+                    if (!_poolCenter.ContainsKey(_configPoolItem[i].ItemName))
+                    {
+                        //当前对象池未存在当前对象
+                        _poolCenter.Add(_configPoolItem[i].ItemName , new Queue<GameObject>());
+                        _poolCenter[_configPoolItem[i].ItemName].Enqueue(item);
+                    }
+                    else
+                    {
+                        _poolCenter[_configPoolItem[i].ItemName].Enqueue(item);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取对象池对象
+        /// </summary>
+        /// <param name="itemName"></param>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        public void TryGetPoolItem(string itemName , Vector3 position , Quaternion rotation)
+        {
+            if (_poolCenter.ContainsKey(itemName))
+            {
+                var item = _poolCenter[name].Dequeue();
+                item.transform.position = position;
+                item.transform.rotation = rotation;
+                item.SetActive(true);
+                _poolCenter[name].Enqueue(item);
+            }
+            else
+            {
+                Debug.Log("当前池子不存在" + name);
+            }
+        }
+        
+        public GameObject TryGetPoolItem(string itemName)
+        {
+            if (_poolCenter.ContainsKey(itemName))
+            {
+                var item = _poolCenter[name].Dequeue(); 
+                item.SetActive(true);
+                _poolCenter[name].Enqueue(item);
+                return item;
+            }
+            
+            Debug.Log("当前池子不存在" + name);
+
+            return null;
+        }
     }
 }
