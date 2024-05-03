75bbddf9b5a2a72bdb8c6354cdfece143464cf53
Soraku7 <2673140287@qq.com>
2024-05-04 00:46:34 +0800

    EnemyMove

---
+++ Assets/Scripts/Character/Enemy/Move/EnemyMoveControl.cs
@@ -0,0 +1,64 @@
+﻿using Base;
+using GGG.Tool;
+using Manager;
+using UnityEngine;
+
+namespace Character.Enemy.Move
+{
+    public class EnemyMoveControl : CharacterMovementControlBase
+    {
+        private bool _applyMovement;
+
+
+        protected override void Start()
+        {
+            base.Start();
+            SetApplyMovement(true);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            LockTargetDirection();
+        }
+        
+        //玩家在移动的时候观看AI
+        private void LockTargetDirection()
+        {
+            if (Anim.AnimationAtTag("Motion"))
+            {
+                transform.Look(EnemyManager.MainInstance.GetMainPlayer().position , 500f );
+            }
+        }
+        
+        /// <summary>
+        /// 设置动画移动参数
+        /// </summary>
+        /// <param name="horizontal"></param>
+        /// <param name="vertical"></param>
+        public void SetAnimatorMovementValue(float horizontal, float vertical)
+        {
+            if(_applyMovement)
+            {
+                Anim.SetFloat(AnimationID.LockID, 1f);
+                Anim.SetFloat(AnimationID.HasInputID, horizontal, 0.2f,
+                    Time.deltaTime);
+                Anim.SetFloat(AnimationID.VerticalID, vertical, 0.2f,
+                    Time.deltaTime);
+            }
+            else
+            {
+                Anim.SetFloat(AnimationID.LockID, 0f);
+                Anim.SetFloat(AnimationID.HasInputID, horizontal, 0f,
+                    Time.deltaTime);
+                Anim.SetFloat(AnimationID.VerticalID, vertical, 0f,
+                    Time.deltaTime);
+            }
+        }
+
+        public void SetApplyMovement(bool apply)
+        {
+            _applyMovement = apply;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -0,0 +1,19 @@
+﻿using Unilts.Tools.Singleton;
+using UnityEngine;
+
+namespace Manager
+{
+    public class EnemyManager : Singleton<EnemyManager>
+    {
+        private Transform _mainPlayer;
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _mainPlayer = GameObject.FindWithTag("Player").transform;
+        }
+
+        public Transform GetMainPlayer() => _mainPlayer;
+    }
+}
