b0b6228888df18489f5a6299c38d851c5ccff0ff
Soraku7 <2673140287@qq.com>
2024-06-03 01:30:29 +0800

    AI Free walk

--- Assets/Scripts/Character/Enemy/Move/AIFreeMovementAction.cs
+++ Assets/Scripts/Character/Enemy/Move/AIFreeMovementAction.cs
@@ -11,6 +11,16 @@ namespace Character.Enemy.Move
         private EnemyMoveControl _enemyMoveControl;
         private EnemyCombatControl _enemyCombatControl;
 
+        /// <summary>
+        /// 动作索引
+        /// </summary>
+        private int _actionIndex;
+        /// <summary>
+        /// 上个动作的索引
+        /// </summary>
+        private int _lastActionIndex;
+        private float _actionTimer;
+
         public override void OnAwake()
         {
             base.OnAwake();
@@ -24,14 +34,18 @@ namespace Character.Enemy.Move
             if (!_enemyCombatControl.GetAttackCommand())
             {
                 
-                if (DistanceForTarget() > 0.8f)
+                if (DistanceForTarget() > 8f)
                 {
                     _enemyMoveControl.SetAnimatorMovementValue(0f , 1f);
                 }
+                else if(DistanceForTarget() < 8.0f && DistanceForTarget() > 3.0f)
+                {
+                    FreeMovement();
+                    UpdateFreeAction();
+                }
                 else
                 {
-                    Debug.Log(DistanceForTarget());
-                    _enemyMoveControl.SetAnimatorMovementValue(0f , 0f);
+                    _enemyMoveControl.SetAnimatorMovementValue(-1f , -1f);
                 }
                 
                 return TaskStatus.Running;
@@ -47,5 +61,64 @@ namespace Character.Enemy.Move
         private float DistanceForTarget() => DevelopmentToos.DistanceForTarget(
             EnemyManager.MainInstance.GetMainPlayer(),
             _enemyMoveControl.transform);
+
+        private void FreeMovement()
+        {
+            switch (_actionIndex)
+            {
+                case 0:
+                    //向左移动
+                    _enemyMoveControl.SetAnimatorMovementValue(-1 , 0);
+                    break;
+                
+                case 1:
+                    //向右移动
+                    _enemyMoveControl.SetAnimatorMovementValue(0 , 1);
+                    break;
+                
+                case 2:
+                    //向右移动
+                    _enemyMoveControl.SetAnimatorMovementValue(0 , 0);
+                    break;
+                
+                case 3:
+                    //向右移动
+                    _enemyMoveControl.SetAnimatorMovementValue(-1 , -1);
+                    break;
+                
+                case 4:
+                    //向右移动
+                    _enemyMoveControl.SetAnimatorMovementValue(1 , 1);
+                    break;
+            }
+        }
+
+        private void UpdateFreeAction()
+        {
+            if (_actionTimer > 0)
+            {
+                _actionTimer -= Time.deltaTime;
+                if (_actionTimer <= 0)
+                {
+                    UpdateActionIndex();
+                }
+            }
+        }
+
+        private void UpdateActionIndex()
+        {
+            _lastActionIndex = _actionIndex;
+            _actionIndex = Random.Range(0, 5);
+            _actionTimer = Random.Range(2, 3);
+            if (_actionIndex == _lastActionIndex)
+            {
+                _actionIndex = Random.Range(0, 2);
+            }
+
+            if (_actionIndex == 3 || _actionIndex == 4)
+            {
+                
+            }
+        }
     }
 }
